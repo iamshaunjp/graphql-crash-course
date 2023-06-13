240832660672dacaa98c2ae41d189f48f7e0a92a
Shaun <shaun@thenetninja.co.uk>
2023-06-13 11:28:50 +0100

    lesson-9

--- index.js
+++ index.js
@@ -61,6 +61,17 @@ const resolvers = {
       db.games = db.games.filter((g) => g.id !== args.id)
 
       return db.games
+    },
+    updateGame(_, args) {
+      db.games = db.games.map((g) => {
+        if (g.id === args.id) {
+          return {...g, ...args.edits}
+        }
+
+        return g
+      })
+
+      return db.games.find((g) => g.id === args.id)
     }
   }
 }
--- schema.js
+++ schema.js
@@ -29,9 +29,14 @@ export const typeDefs = `#graphql
   type Mutation {
     addGame(game: AddGameInput!): Game
     deleteGame(id: ID!): [Game]
+    updateGame(id: ID!, edits: EditGameInput): Game
   }
   input AddGameInput {
     title: String!,
     platform: [String!]!
+  }
+  input EditGameInput {
+    title: String,
+    platform: [String!]
   }
 `
