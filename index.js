658e7ff2236da24eeef18c6d143403f6695a6526
Shaun <shaun@thenetninja.co.uk>
2023-06-13 10:35:55 +0100

    lesson-5

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 
+// data
+import db from './_db.js'
+
 // types
 import { typeDefs } from './schema.js'
 
+// resolvers
+const resolvers = {
+  Query: {
+    games() {
+      return db.games
+    },
+    authors() {
+      return db.authors
+    },
+    reviews() {
+      return db.reviews
+    },
+  }
+}
+
 // server setup
 const server = new ApolloServer({
-  typeDefs
-  // resolvers
+  typeDefs,
+  resolvers
 })
 
 const { url } = await startStandaloneServer(server, {
