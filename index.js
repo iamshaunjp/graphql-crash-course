8122c7f5a4190a36917a10b1079a5a1ca9c82295
Root99 <azizrezgui4@gmail.com>
2023-06-29 13:42:00 +0100

    basic server setup

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
-import { ApolloServer } from '@apollo/server'
-import { startStandaloneServer } from '@apollo/server/standalone'
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
+const typeDefs = `#graphql
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: number
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
 // server setup
 const server = new ApolloServer({
-  // typeDefs
-  // resolvers
-})
+  typeDefs,
+  resolvers,
+});
 
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 }
-})
+  listen: { port: 4000 },
+});
 
-console.log(`Server ready at: ${url}`)
+console.log(`Server ready at: ${url}`);
