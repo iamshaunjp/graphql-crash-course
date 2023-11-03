eeeb3526aebad562a86681347e607da548df2f09
Barack _ Ouma <127542979+IsoDevMate@users.noreply.github.com>
2023-11-03 16:57:28 +0300

    nested-quesries

--- index.js
+++ index.js
@@ -13,13 +13,40 @@ const resolvers = {
     games() {
       return db.games
     },
+    game(_, { id }) {
+      return db.games.find(game => game.id === id)
+    },
     authors() {
       return db.authors
+    },
+    author(_, { id }) {
+      return db.authors.find(author => author.id === id)
     },
     reviews() {
       return db.reviews
     },
-  }
+    review(_, { id }) {
+      return db.reviews.find(review => review.id === id)
+    },
+  },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter(review => review.game_id === parent.id)
+    },
+  Author: {
+      reviews(parent) {
+        return db.reviews.filter(review => review.author_id === parent.id)
+      }
+    },
+  Review: {
+      game(parent) {
+        return db.games.find(game => game.id === parent.game_id)
+      },
+      author(parent) {
+        return db.authors.find(author => author.id === parent.author_id)
+      }
+    },
+  },
 }
 
 // server setup
--- schema.js
+++ schema.js
@@ -3,20 +3,28 @@ export const typeDefs = `#graphql
     id: ID!
     title: String!
     platform: [String!]!
+    reviews: [Review!]
   }
   type Review {
     id: ID!
     rating: Int!
     content: String!
+    game: Game!
+    author: Author!
+
   }
   type Author {
     id: ID!
     name: String!
     verified: Boolean!
+    reviews: [Review!]
   }
   type Query {
     games: [Game]
+    game(id: ID!): Game
     reviews: [Review]
+    review(id: ID!): Review
     authors: [Author]
+    author(id: ID!): Author
   }
 `
